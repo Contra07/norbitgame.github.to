39bf0b4c7bcd06aa33f35cde6ed5b875c15345c3
Contra <nano2012rus@yandex.ru>
2023-06-23 21:36:17 +0400

    Delta time and FPS counter

--- game.js
+++ game.js
@@ -7,30 +7,52 @@ class Plane{
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
     }
+
+    Clear() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    DrawText(text,x,y){
+        this.ctx.font = "20px serif";
+        this.ctx.fillText(text,x,y)
+    }
+
+    DrawSquare(square){
+        this.ctx.fillRect(square.x*10, square.y, square.h, square.w);
+    }
+
+}
+
+class Square{
+    constructor(x,y,h,w){
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.w = w;
+    }
 }
 
 class Game {
     constructor(canvas) {
         this.window = new Plane(canvas);
+        this.time = 0;
+        this.fps = 0;
+        this.square = new Square(10,100,20,20)
     }  
     
     Init(){
-        this.window.ctx.fillRect(20, 10, 150, 100);
+        this.time = performance.now()
     }
 
-
-    Update() {
-        
+    Update(dt) {
+        this.square.x += 0.1;
     }
 
     Draw(){
-        ClearCanvas();
-        background.Draw();
-        player.Draw();
-    }
-
-    ClearCanvas() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.window.Clear();
+        //this.window.ctx.fillRect(20, 10, 150, 100);
+        this.window.DrawText(this.fps,20,20)
+        this.window.DrawSquare(this.square);
     }
 
     keyDownListener(e) {
@@ -40,5 +62,17 @@ class Game {
     }
 }
 
-(new Game(document.getElementById('mycanvas'))).Init();
+function GameLoop(timeStamp){
+    dt = timeStamp - game.time;
+    game.time = timeStamp;
+    game.fps = 1 / (dt / 1000);
+    game.Update(dt);
+    game.Draw();
+    requestAnimationFrame(this.GameLoop);
+}
+
+
+let game = new Game(document.getElementById('mycanvas'));
+game.Init();
+GameLoop();
 
